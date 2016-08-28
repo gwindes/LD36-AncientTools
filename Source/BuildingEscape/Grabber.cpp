484b2b677698e59eb5b0e2daa21ae1868ac57b8d
Grant Windes <gwindes@gmail.com>
2016-08-27 22:40:38 -0700

    Add phsyics handle component.

--- Source/BuildingEscape/Grabber.cpp
+++ Source/BuildingEscape/Grabber.cpp
@@ -22,6 +22,11 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 
 	UE_LOG(LogTemp, Warning, TEXT("Grabber reporting for duty!"));
+
+	PhysicsHandle = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (!PhysicsHandle) {
+		UE_LOG(LogTemp, Error, TEXT("%s is missing phsics handle component"), *GetOwner()->GetName());
+	}
 }
 
 
--- Source/BuildingEscape/Grabber.h
+++ Source/BuildingEscape/Grabber.h
@@ -23,5 +23,7 @@ public:
 
 private:
 	float Reach = 100.f;
+
+	UPhysicsHandleComponent * PhysicsHandle = nullptr;
 	
 };
